40f3bd1b06b0eddb6770d6e308ee9d6b6a233e42
Fumiya Fujinaka <uzuna.kf@gmail.com>
2019-01-19 18:57:47 +0900

    add lock and read sequence

--- twlock.go
+++ twlock.go
@@ -1,15 +1,100 @@
 package twlock
 
-import "context"
+import (
+	"context"
+	"sync"
+)
 
 // あるリソースへのアクセスを表現するInterface
 type ResourceFunc func(ctx context.Context, req interface{}) (res interface{}, ok bool, err error)
 
 // リクエストの管理グループ名を返す
-type GroupFunc func(ctx context.Context, req interface{}) string
+type GroupFunc func(req interface{}) string
+
+type CacheStore interface {
+	Get(ctx context.Context, req interface{}) (res interface{}, ok bool, err error)
+	Set(req interface{}, res interface{}) error
+}
+
+func NewTWLock(g GroupFunc, c CacheStore, o ResourceFunc) *TWLock {
+	return &TWLock{
+		groupFunc:  g,
+		cacheFunc:  c,
+		originFunc: o,
+		originLock: make(map[string]struct{}),
+		cacheLock:  make(map[string]*sync.RWMutex),
+		mux:        new(sync.Mutex),
+	}
+}
 
 type TWLock struct {
 	groupFunc  GroupFunc
-	cacheFunc  ResourceFunc
+	cacheFunc  CacheStore
 	originFunc ResourceFunc
+	originLock map[string]struct{}
+	cacheLock  map[string]*sync.RWMutex
+	mux        *sync.Mutex
+}
+
+// Request Sequence
+func (l *TWLock) In(ctx context.Context, req interface{}) (interface{}, error) {
+	// get cache group
+	grName := l.groupFunc(req)
+
+	// create cache lock
+	cl, ok := l.cacheLock[grName]
+	if !ok {
+		l.mux.Lock()
+		if l.cacheLock[grName] == nil {
+			l.cacheLock[grName] = new(sync.RWMutex)
+		}
+		cl = l.cacheLock[grName]
+		l.mux.Unlock()
+	}
+
+	for {
+		// cache read and wait
+		cl.RLock()
+		res, ok, err := l.cacheFunc.Get(ctx, req)
+		if err != nil {
+			cl.RUnlock()
+			return nil, err
+		}
+		if ok {
+			cl.RUnlock()
+			return res, nil
+		}
+
+		// When has not cache data
+		// 1 request through to origin resource and lock resource.
+		l.mux.Lock()
+		if _, ok := l.originLock[grName]; ok {
+			// If request isafter resource mutex flow that back to read lock
+			l.mux.Unlock()
+			cl.RUnlock()
+			continue
+		}
+		l.originLock[grName] = struct{}{}
+		l.mux.Unlock()
+		cl.RUnlock()
+		break
+	}
+	// Origin request
+	cl.Lock()
+	defer func() {
+		cl.Unlock()
+		delete(l.originLock, grName)
+	}()
+
+	// origin route確認
+	res, ok, err := l.originFunc(ctx, req)
+	if err != nil {
+		return nil, err
+	}
+	if !ok {
+		return res, nil
+	}
+	// set cache
+	err = l.cacheFunc.Set(req, res)
+	return res, err
 }
--- twlock_test.go
+++ twlock_test.go
@@ -2,43 +2,135 @@ package twlock_test
 
 import (
 	"context"
+	"log"
+	"sync"
 	"testing"
 	"time"
+
+	"github.com/stretchr/testify/assert"
+	twlock "github.com/uzuna/go-twlock"
 )
 
-func TestTway(t testing.T) {
+func TestTway(t *testing.T) {
+	g := func(req interface{}) string {
+		x := req.(*DummyRequest)
+		return x.Name
+	}
 
+	c := newDummyCache()
+	o := &DummyOrigin{
+		mutex: new(sync.Mutex),
+	}
+
+	l := twlock.NewTWLock(g, c, o.Get)
+
+	wg := &sync.WaitGroup{}
+	f := func(i int) {
+		defer wg.Done()
+		req := &DummyRequest{
+			Name: "X",
+			Wait: time.Millisecond * time.Duration(i) * 100,
+			Has:  true,
+		}
+		ctx := context.Background()
+		res, err := l.In(ctx, req)
+		if err != nil {
+			log.Println(res, err)
+		}
+	}
+	length := 10000
+	for i := 0; i < length; i++ {
+		wg.Add(1)
+		go f(i)
+	}
+	wg.Wait()
+
+	assert.Equal(t, length, c.counter)
+	assert.Equal(t, 1, o.counter)
+
+	// table := []*DummyRequest{
+	// 	&DummyRequest{
+	// 		Name: "X",
+	// 		Wait: time.Millisecond * 10,
+	// 		Has:  true,
+	// 	},
+	// }
+
+	// for _, v := range table {
+	// 	ctx := context.Background()
+	// 	res, err := l.In(ctx, v)
+	// 	log.Println(res, err)
+	// }
+}
+
+func newDummyCache() *DummyCache {
+	return &DummyCache{
+		data:      make(map[string]*DummyRequest),
+		mutex:     new(sync.Mutex),
+		cacheLock: new(sync.RWMutex),
+	}
 }
 
 type DummyCache struct {
-	data map[string]interface{}
+	data      map[string]*DummyRequest
+	mutex     *sync.Mutex
+	cacheLock *sync.RWMutex
+	counter   int
 }
 
-func (d *DummyCache) Set(key string, v interface{}, expire time.Duration) error {
+func (d *DummyCache) Set(req interface{}, res interface{}) error {
+	x := req.(*DummyRequest)
+	d.cacheLock.Lock()
+	d.data[x.Name] = x
+	d.cacheLock.Unlock()
+	go func() {
+		log.Println("Cache")
+		time.Sleep(time.Millisecond)
+		d.cacheLock.Lock()
+		delete(d.data, x.Name)
+		d.cacheLock.Unlock()
+	}()
 	return nil
 }
-func (d *DummyCache) Get(ctx context.Context, key string) (v interface{}, ok bool, err error) {
-	return nil, false, nil
+func (d *DummyCache) Get(ctx context.Context, req interface{}) (v interface{}, ok bool, err error) {
+	d.mutex.Lock()
+	d.counter++
+	d.mutex.Unlock()
+	x := req.(*DummyRequest)
+	d.cacheLock.RLock()
+	defer d.cacheLock.RUnlock()
+	if v, ok := d.data[x.Name]; ok {
+		return v, true, nil
+	}
+	return nil, ok, nil
 }
 
 type DummyRequest struct {
-	Wait   time.Duration
-	HasNot bool
-	Error  error
+	Name  string
+	Wait  time.Duration
+	Has   bool
+	Error error
 }
 
-type DummyOrigin struct{}
+type DummyOrigin struct {
+	mutex   *sync.Mutex
+	counter int
+}
 
-func (d *DummyOrigin) Get(ctx context.Context, x DummyRequest) (v interface{}, ok bool, err error) {
+func (d *DummyOrigin) Get(ctx context.Context, v interface{}) (interface{}, bool, error) {
+	d.mutex.Lock()
+	d.counter++
+	d.mutex.Unlock()
+	x := v.(*DummyRequest)
 	if x.Error != nil {
-		return nil, ok, x.Error
+		return nil, false, x.Error
 	}
 
-	timer := time.NewTimer(x.Duration)
+	timer := time.NewTimer(x.Wait)
 	select {
 	case <-ctx.Done():
-		return nil, ok, ctx.Err()
+		return nil, false, ctx.Err()
 	case <-timer.C:
-		return x.Wait, x.HasNot, nil
+		return x.Wait, x.Has, nil
 	}
 }
